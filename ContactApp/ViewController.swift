8c4f5c913497d6ce5952a695988859c6f70bc6ad
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-02-24 19:49:22 +0500

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by A'zamjon Abdumuxtorov on 24/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
